3e31b2f2883cfa136775ddcc472fdd7d8662a4e3
Marcin Meinardi <mmeinardi@psop.pl>
2017-04-02 20:53:46 +0200

    setup.py upgrade.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='sql-schema-builder',
@@ -6,7 +6,7 @@ setup(
     version='1.0.0',
     description='Library to keep SQL database schema up to date with your code.',
     author='Ganymede',
-    author_email='cerebro@ganymede.eu',
+    author_email='mmeinardi@ganymede.eu',
     license='Apache License, Version 2.0',
     url='https://github.com/mmeinardi-ganymede/sql-schema-builder',
     download_url='https://github.com/mmeinardi-ganymede/sql-schema-builder/tarball/1.0.0',
