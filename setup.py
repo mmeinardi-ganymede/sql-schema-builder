7d881a4b91e7935dd72d50341e3cfa393f53e886
Marcin Meinardi <mmeinardi@psop.pl>
2017-04-02 21:03:05 +0200

    Fix deployment issues.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='sql-schema-builder',
     packages = ['sql_schema_builder'],
-    version='1.0.0',
+    version='1.0.1',
     description='Library to keep SQL database schema up to date with your code.',
     author='Ganymede',
     author_email='mmeinardi@ganymede.eu',
     license='Apache License, Version 2.0',
     url='https://github.com/mmeinardi-ganymede/sql-schema-builder',
-    download_url='https://github.com/mmeinardi-ganymede/sql-schema-builder/tarball/1.0.0',
+    download_url='https://github.com/mmeinardi-ganymede/sql-schema-builder/tarball/1.0.1',
     keywords=['pymysql sql schema builder mysql ddl migration migrate'],
     install_requires=['pymysql']
 )
